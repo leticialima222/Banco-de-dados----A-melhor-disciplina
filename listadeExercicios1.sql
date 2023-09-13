6d2f4316f4831b2f1152b797edb8c630b5f981fa
leticialima222 <143537892+leticialima222@users.noreply.github.com>
2023-09-12 21:20:22 -0300

    ex 3

--- listadeExercicios1.sql
+++ listadeExercicios1.sql
@@ -1,2 +1,7 @@
+
 SELECT titulo FROM livros;
 SELECT nome, nascimento FROM autores WHERE YEAR(nascimento) < 1900;
+SELECT l.titulo
+FROM livros l
+JOIN autores a ON l.autor_id = a.id
+WHERE a.nome = 'J.K. Rowling';

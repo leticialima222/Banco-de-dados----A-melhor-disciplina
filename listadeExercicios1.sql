7291dac17d21ae84dff04ef3e67491036a548142
leticialima222 <143537892+leticialima222@users.noreply.github.com>
2023-09-12 21:38:58 -0300

    ex 20

--- listadeExercicios1.sql
+++ listadeExercicios1.sql
@@ -66,3 +66,8 @@ SELECT alunos.nome, COUNT(matriculas.curso) as numero_matriculas
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
 GROUP BY alunos.nome;
+SELECT produto, COUNT(*) as quantidade_transacoes
+FROM vendas
+GROUP BY produto
+ORDER BY quantidade_transacoes DESC
+LIMIT 1;

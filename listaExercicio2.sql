5cd3f76fee728b1e61008d11727305c0127de1f6
leticialima222 <143537892+leticialima222@users.noreply.github.com>
2023-09-26 22:27:50 -0300

    2

--- listaExercicio2.sql
+++ listaExercicio2.sql
@@ -5,3 +5,17 @@ BEGIN
 END //
 DELIMITER ;
 CALL sp_ListarAutores();
+
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo, Autor.Nome, Autor.Sobrenome
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID
+    WHERE Categoria.Nome = categoria_nome;
+END //
+DELIMITER ;
+CALL sp_LivrosPorCategoria('Romance');

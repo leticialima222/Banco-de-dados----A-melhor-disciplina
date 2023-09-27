6d0e70376e524f81dee6bd535b3e73f9f0ed3ce1
leticialima222 <143537892+leticialima222@users.noreply.github.com>
2023-09-26 22:32:16 -0300

    6

--- listaExercicio2.sql
+++ listaExercicio2.sql
@@ -67,3 +67,30 @@ END //
 
 DELIMITER ;
 CALL sp_LivrosAteAno(2010);
+
+
+DELIMITER //
+CREATE PROCEDURE sp_TitulosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    DECLARE done INT DEFAULT 0;
+    DECLARE livro_titulo VARCHAR(255);
+    DECLARE cur CURSOR FOR
+        SELECT Livro.Titulo
+        FROM Livro
+        INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+        WHERE Categoria.Nome = categoria_nome;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN cur;
+    FETCH cur INTO livro_titulo;
+
+    WHILE NOT done DO
+        SELECT livro_titulo AS 'Título do Livro';
+        FETCH cur INTO livro_titulo;
+    END WHILE;
+
+    CLOSE cur;
+END //
+
+DELIMITER ;
+CALL sp_TitulosPorCategoria('Romance');

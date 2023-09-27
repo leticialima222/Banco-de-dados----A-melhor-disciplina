aaf0c133e65222d5fd7a4ca1735a9b2054e99f2c
leticialima222 <143537892+leticialima222@users.noreply.github.com>
2023-09-26 22:28:27 -0300

    3

--- listaExercicio2.sql
+++ listaExercicio2.sql
@@ -19,3 +19,17 @@ BEGIN
 END //
 DELIMITER ;
 CALL sp_LivrosPorCategoria('Romance');
+
+DELIMITER //
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoria_nome VARCHAR(100), OUT total_livros INT)
+BEGIN
+    SELECT COUNT(*) INTO total_livros
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoria_nome;
+END //
+    
+DELIMITER ;
+SET @resultado = 0;
+CALL sp_ContarLivrosPorCategoria('Romance', @resultado);
+SELECT @resultado;
